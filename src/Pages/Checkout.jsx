76ec4806fe3125c075ff80e4c8a0891bdf57f7d8
Talha Zaafar <talhazafarmustafa@gmail.com>
2026-08-14 17:25:01 +0500

    File Updated

--- src/Pages/Checkout.jsx
+++ src/Pages/Checkout.jsx
@@ -1,15 +1,24 @@
+import Footer from "../Footer/Footer"
 import Hero31 from "../Sections/Hero31"
 
-
+import { useEffect } from "react"
 
 
 
 
 function Checkout() {
+
+useEffect(()=>{
+window.scrollTo(0,0)
+},[])
   return (
     <div>
 
     <Hero31 />
+
+     <div className="fti">
+        <Footer />
+        </div>
       
     </div>
   )

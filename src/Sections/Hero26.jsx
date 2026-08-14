50f9c5caca47d85f702b59943dcb2b918944b0ac
Talha Zaafar <talhazafarmustafa@gmail.com>
2026-08-14 15:31:32 +0500

    File Updated

--- src/Sections/Hero26.jsx
+++ src/Sections/Hero26.jsx
@@ -12,7 +12,7 @@ id:61,
 image:"https://chawkbazar.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2Fp-10.png&w=384&q=100",
 title:"Nike Shoes",
 description:"NIKE 2020 Black White is a clean and monochromatic colourway of the label’s latest high-technology silhouette. The model first launched late last year and is currently Jordan Brand’s flagship performance pair.",
-newPrice:"$50",
+newPrice:50,
 },
 
 {
@@ -20,8 +20,8 @@ id:62,
 image:"https://chawkbazar.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2Fp-5.png&w=384&q=100",
 title:"12 Eco-Friendly Clothing Brands",
 description:"12 Eco-Friendly Clothing Brands That, has a scoop neck, sleeveless, straight hem",
-newPrice:"$12",
-oldPrice:"$28",
+newPrice:12,
+oldPrice:28,
 },
 
 {
@@ -29,8 +29,8 @@ id:63,
 image:"https://chawkbazar.vercel.app/assets/images/products/p-22-m.png",
 title:"H&M Global Desi",
 description:"Blue solid woven regular top, curved hem with tassell detailing has shoulder straps, and sleeveless",
-newPrice:"$13",
-oldPrice:"$24",
+newPrice:13,
+oldPrice:24,
 },
 
 {
@@ -38,8 +38,8 @@ id:64,
 image:"https://chawkbazar.vercel.app/assets/images/products/p-17-m.png",
 title:"Short Sleeve Shirts For Men",
 description:"From casual days out to parties, dinners and other events that call for a dressier look, a short sleeve shirt is a versatile piece that works for all kinds of occasions.",
-newPrice:"$125",
-oldPrice:"$118",
+newPrice:125,
+oldPrice:118,
 },
 
 
@@ -48,8 +48,8 @@ id:65,
 image:"https://chawkbazar.vercel.app/assets/images/products/p-7-m.png",
 title:"Regular Fit Crew-neck T-shirt",
 description:"From casual days out to parties, dinners and other events that call for a dressier look, a short sleeve shirt is a versatile piece that works for all kinds of occasions.",
-newPrice:"$8",
-oldPrice:"$6",
+newPrice:8,
+oldPrice:6,
 },
 
 
@@ -58,8 +58,8 @@ id:66,
 image:"https://chawkbazar.vercel.app/assets/images/products/p-15-m.png",
 title:"Scuba Stand Collar Topper Jacket",
 description:"Self-striped knitted midi A-line dress, has a scoop neck, T-shirt, straight hem",
-newPrice:"$44",
-oldPrice:"$34",
+newPrice:44,
+oldPrice:34,
 },
 
 {
@@ -67,8 +67,8 @@ id:67,
 image:"https://chawkbazar.vercel.app/assets/images/products/p-8-m.png",
 title:"Gucci Carlton UK",
 description:'Zara provides only the highest-quality selection of dresses, womens suits, and suited separates.',
-newPrice:"$20",
-oldPrice:"12",
+newPrice:20,
+oldPrice:12,
 },
 
 
@@ -77,8 +77,8 @@ id:68,
 image:"https://chawkbazar.vercel.app/assets/images/products/p-3-m.png",
 title:"Zara Shoes Green",
 description:"Knitted midi A-line dress, has a scoop neck, sleeveless, straight hem",
-newPrice:"$10",
-oldPrice:"$3",
+newPrice:10,
+oldPrice:3,
 },
 
 
@@ -87,8 +87,8 @@ id:69,
 image:"https://chawkbazar.vercel.app/assets/images/products/p-18-m.png",
 title:"Zara Solly White Shirt",
 description:"Footwear refers to garments worn on the feet, which originally serves to purpose of protection against adversities of the environment, usually regarding ground textures and temperature.",
-newPrice:"$5",
-oldPrice:"$6",
+newPrice:5,
+oldPrice:6,
 },
 
 
@@ -97,8 +97,8 @@ id:70,
 image:"https://chawkbazar.vercel.app/assets/images/products/p-19-m.png",
 title:"Zara Shoes Green",
 description:"Footwear refers to garments worn on the feet, which originally serves to purpose of protection against adversities of the environment, usually regarding ground textures and temperature.",
-newPrice:"$3",
-oldPrice:"$22",
+newPrice:3,
+oldPrice:22,
 },
 
 
@@ -107,8 +107,8 @@ id:71,
 image:"https://chawkbazar.vercel.app/assets/images/products/p-9-m.png",
 title:"Women Fitted Neck T-Shirt",
 description:"Footwear refers to garments worn on the feet, which originally serves to purpose of protection against adversities of the environment, usually regarding ground textures and temperature.",
-newPrice:"$128",
-oldPrice:"$130",
+newPrice:128,
+oldPrice:130,
 
 },
 
@@ -117,7 +117,7 @@ id:72,
 image:"https://chawkbazar.vercel.app/assets/images/products/p-4-m.png",
 title:"Regular Fit Flannel Shirt",
 description:"Footwear refers to garments worn on the feet, which originally serves to purpose of protection against adversities of the environment, usually regarding ground textures and temperature.",
-newPrice:"$120",
+newPrice:120,
 }
 ]
 
--- src/Sections/Hero27.jsx
+++ src/Sections/Hero27.jsx
@@ -24,7 +24,7 @@ id:61,
 image:"https://chawkbazar.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2Fp-10.png&w=384&q=100",
 title:"Nike Shoes",
 description:"NIKE 2020 Black White is a clean and monochromatic colourway of the label’s latest high-technology silhouette. The model first launched late last year and is currently Jordan Brand’s flagship performance pair.",
-newPrice:"$50",
+newPrice:50,
 },
 
 {
@@ -32,8 +32,8 @@ id:62,
 image:"https://chawkbazar.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2Fp-5.png&w=384&q=100",
 title:"12 Eco-Friendly Clothing Brands",
 description:"12 Eco-Friendly Clothing Brands That, has a scoop neck, sleeveless, straight hem",
-newPrice:"$12",
-oldPrice:"$28",
+newPrice:12,
+oldPrice:28,
 },
 
 {
@@ -41,8 +41,8 @@ id:63,
 image:"https://chawkbazar.vercel.app/assets/images/products/p-22-m.png",
 title:"H&M Global Desi",
 description:"Blue solid woven regular top, curved hem with tassell detailing has shoulder straps, and sleeveless",
-newPrice:"$13",
-oldPrice:"$24",
+newPrice:13,
+oldPrice:24,
 },
 
 {
@@ -50,8 +50,8 @@ id:64,
 image:"https://chawkbazar.vercel.app/assets/images/products/p-17-m.png",
 title:"Short Sleeve Shirts For Men",
 description:"From casual days out to parties, dinners and other events that call for a dressier look, a short sleeve shirt is a versatile piece that works for all kinds of occasions.",
-newPrice:"$125",
-oldPrice:"$118",
+newPrice:125,
+oldPrice:118,
 },
 
 
@@ -60,8 +60,8 @@ id:65,
 image:"https://chawkbazar.vercel.app/assets/images/products/p-7-m.png",
 title:"Regular Fit Crew-neck T-shirt",
 description:"From casual days out to parties, dinners and other events that call for a dressier look, a short sleeve shirt is a versatile piece that works for all kinds of occasions.",
-newPrice:"$8",
-oldPrice:"$6",
+newPrice:8,
+oldPrice:6,
 },
 
 
@@ -70,8 +70,8 @@ id:66,
 image:"https://chawkbazar.vercel.app/assets/images/products/p-15-m.png",
 title:"Scuba Stand Collar Topper Jacket",
 description:"Self-striped knitted midi A-line dress, has a scoop neck, T-shirt, straight hem",
-newPrice:"$44",
-oldPrice:"$34",
+newPrice:44,
+oldPrice:34,
 },
 
 {
@@ -79,8 +79,8 @@ id:67,
 image:"https://chawkbazar.vercel.app/assets/images/products/p-8-m.png",
 title:"Gucci Carlton UK",
 description:'Zara provides only the highest-quality selection of dresses, womens suits, and suited separates.',
-newPrice:"$20",
-oldPrice:"12",
+newPrice:20,
+oldPrice:12,
 },
 
 
@@ -89,8 +89,8 @@ id:68,
 image:"https://chawkbazar.vercel.app/assets/images/products/p-3-m.png",
 title:"Zara Shoes Green",
 description:"Knitted midi A-line dress, has a scoop neck, sleeveless, straight hem",
-newPrice:"$10",
-oldPrice:"$3",
+newPrice:10,
+oldPrice:3,
 },
 
 
@@ -99,8 +99,8 @@ id:69,
 image:"https://chawkbazar.vercel.app/assets/images/products/p-18-m.png",
 title:"Zara Solly White Shirt",
 description:"Footwear refers to garments worn on the feet, which originally serves to purpose of protection against adversities of the environment, usually regarding ground textures and temperature.",
-newPrice:"$5",
-oldPrice:"$6",
+newPrice:5,
+oldPrice:6,
 },
 
 
@@ -109,8 +109,8 @@ id:70,
 image:"https://chawkbazar.vercel.app/assets/images/products/p-19-m.png",
 title:"Zara Shoes Green",
 description:"Footwear refers to garments worn on the feet, which originally serves to purpose of protection against adversities of the environment, usually regarding ground textures and temperature.",
-newPrice:"$3",
-oldPrice:"$22",
+newPrice:3,
+oldPrice:22,
 },
 
 
@@ -119,8 +119,8 @@ id:71,
 image:"https://chawkbazar.vercel.app/assets/images/products/p-9-m.png",
 title:"Women Fitted Neck T-Shirt",
 description:"Footwear refers to garments worn on the feet, which originally serves to purpose of protection against adversities of the environment, usually regarding ground textures and temperature.",
-newPrice:"$128",
-oldPrice:"$130",
+newPrice:128,
+oldPrice:130,
 
 },
 
@@ -129,7 +129,7 @@ id:72,
 image:"https://chawkbazar.vercel.app/assets/images/products/p-4-m.png",
 title:"Regular Fit Flannel Shirt",
 description:"Footwear refers to garments worn on the feet, which originally serves to purpose of protection against adversities of the environment, usually regarding ground textures and temperature.",
-newPrice:"$120",
+newPrice:120,
 }
 ]
 

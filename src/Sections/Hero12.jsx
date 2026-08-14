943528b8c26cee5ae4c1ca22c3256e0b322b0307
Talha Zaafar <talhazafarmustafa@gmail.com>
2026-08-14 14:35:02 +0500

    File Updated

--- src/Sections/Hero12.jsx
+++ src/Sections/Hero12.jsx
@@ -11,23 +11,21 @@ import { useContext } from "react"
 
 function Hero12() {
 
-const {addToCart} = useContext(cartContext)
+    const { addToCart } = useContext(cartContext)
 
-useEffect(()=>{
-window.scrollTo(0,0)
-},[])
+    useEffect(() => {
+        window.scrollTo(0, 0)
+    }, [])
 
 
     const { id } = useParams()
-
-
     const products = [
 
         {
             id: 1,
             image: "https://chawkbazar.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2Fp-13.png&w=384&q=100",
             title: "Blazer And A Neck Scarf",
-            description:"blue short sleeve basic midi dress featuring a crew neckline in a jersey fabric.",
+            description: "blue short sleeve basic midi dress featuring a crew neckline in a jersey fabric.",
             newPrice: 13,
             oldPrice: 23,
 
@@ -37,7 +35,7 @@ window.scrollTo(0,0)
             id: 2,
             image: "https://chawkbazar.vercel.app/_next/image?url=%2Fassets%2Fimages%2Fproducts%2Fp-5.png&w=384&q=100",
             title: "12 Eco-Friendly Clothing Brands",
-            description:"12 Eco-Friendly Clothing Brands That, has a scoop neck, sleeveless, straight hem",
+            description: "12 Eco-Friendly Clothing Brands That, has a scoop neck, sleeveless, straight hem",
             newPrice: 20,
             oldPrice: 28,
         },
@@ -46,7 +44,7 @@ window.scrollTo(0,0)
             id: 3,
             image: "https://chawkbazar.vercel.app/assets/images/products/p-22-m.png",
             title: "H&M Global Desi",
-            description:"Blue solid woven regular top, curved hem with tassell detailing has shoulder straps, and sleeveless",
+            description: "Blue solid woven regular top, curved hem with tassell detailing has shoulder straps, and sleeveless",
             newPrice: 30,
             oldPrice: 40,
         },
@@ -55,7 +53,7 @@ window.scrollTo(0,0)
             id: 4,
             image: "https://chawkbazar.vercel.app/assets/images/products/p-17-m.png",
             title: "Short Sleeve Shirts For Men",
-            description:"From casual days out to parties, dinners and other events that call for a dressier look, a short sleeve shirt is a versatile piece that works for all kinds of occasions.",
+            description: "From casual days out to parties, dinners and other events that call for a dressier look, a short sleeve shirt is a versatile piece that works for all kinds of occasions.",
             newPrice: 12,
             oldPrice: 18,
         },
@@ -65,7 +63,7 @@ window.scrollTo(0,0)
             id: 5,
             image: "https://chawkbazar.vercel.app/assets/images/products/p-7-m.png",
             title: "Regular Fit Crew-neck T-shirt",
-            description:"Self-striped knitted midi A-line dress, has a scoop neck, T-shirt, straight hem",
+            description: "Self-striped knitted midi A-line dress, has a scoop neck, T-shirt, straight hem",
             newPrice: 12,
             oldPrice: 16,
         },
@@ -84,7 +82,7 @@ window.scrollTo(0,0)
             id: 7,
             image: "https://chawkbazar.vercel.app/assets/images/products/p-8-m.png",
             title: "Gucci Carlton UK",
-            description:"Knitted midi A-line dress, has a scoop neck, sleeveless, straight hem",
+            description: "Knitted midi A-line dress, has a scoop neck, sleeveless, straight hem",
             newPrice: 14,
             oldPrice: 20,
         },
@@ -94,7 +92,7 @@ window.scrollTo(0,0)
             id: 8,
             image: "https://chawkbazar.vercel.app/assets/images/products/p-1-m.png",
             title: "Nike Black",
-            description:"Casual wear (casual attire or clothing) may be a Western code that’s relaxed, occasional, spontaneous and fitted to everyday use. Casual wear became popular within the Western world",
+            description: "Casual wear (casual attire or clothing) may be a Western code that’s relaxed, occasional, spontaneous and fitted to everyday use. Casual wear became popular within the Western world",
             newPrice: 11,
             oldPrice: 35,
         },
@@ -104,7 +102,7 @@ window.scrollTo(0,0)
             id: 9,
             image: "https://chawkbazar.vercel.app/assets/images/products/p-18-m.png",
             title: "Zara Solly White Shirt",
-            description:"For a chic and smart look, don this white shirt from Solly by Allen Solly. Crafted from a cotton-nylon blend with a hint of stretch, this design features a dotted pattern. Wear this 3/4th sleeves shirt with trousers and wedges to a client meeting.",
+            description: "For a chic and smart look, don this white shirt from Solly by Allen Solly. Crafted from a cotton-nylon blend with a hint of stretch, this design features a dotted pattern. Wear this 3/4th sleeves shirt with trousers and wedges to a client meeting.",
             newPrice: 25,
             oldPrice: 32,
         },
@@ -114,7 +112,7 @@ window.scrollTo(0,0)
             id: 10,
             image: "https://chawkbazar.vercel.app/assets/images/products/p-11-m.png",
             title: "Armani Veni Vidi Vici",
-            description:"Fendi began life in 1925 as a fur and leather speciality store in Rome.",
+            description: "Fendi began life in 1925 as a fur and leather speciality store in Rome.",
             newPrice: 18,
             oldPrice: 23,
         },
@@ -124,7 +122,7 @@ window.scrollTo(0,0)
             id: 11,
             image: "https://chawkbazar.vercel.app/assets/images/products/p-6-m.png",
             title: "Oversized W Sweater",
-            description:"Constructed in cotton sweat fabric, this lovely piece, lacus eu mattis auctor, dolor lectus venenatis nulla, at tristique eros sem vel ante. Sed leo enim, iaculis ornare tristique non, vulputate sit amet ante.",
+            description: "Constructed in cotton sweat fabric, this lovely piece, lacus eu mattis auctor, dolor lectus venenatis nulla, at tristique eros sem vel ante. Sed leo enim, iaculis ornare tristique non, vulputate sit amet ante.",
             newPrice: 45,
             oldPrice: 76,
 
@@ -134,7 +132,7 @@ window.scrollTo(0,0)
             id: 12,
             image: "https://chawkbazar.vercel.app/assets/images/products/p-4-m.png",
             title: "Regular Fit Flannel Shirt",
-            description:"The garments labelled as Committed are products that have been produced using sustainable fibers or processes, reducing their environmental impact. Mango's goal is to support the implementation of practices more committed to the environment.",
+            description: "The garments labelled as Committed are products that have been produced using sustainable fibers or processes, reducing their environmental impact. Mango's goal is to support the implementation of practices more committed to the environment.",
             newPrice: 11,
         }
 
@@ -161,7 +159,7 @@ window.scrollTo(0,0)
                         <p>Old Price <span className="old-price">$ {item.oldPrice}</span> </p>
                     )
                 }
-                <button onClick={()=>addToCart(item)}>Add To Cart</button>
+                <button onClick={() => addToCart(item)}>Add To Cart</button>
             </div>
 
 
--- src/Sections/Hero16.jsx
+++ src/Sections/Hero16.jsx
@@ -1,11 +1,6 @@
 import { Link } from "react-router-dom"
 
 
-
-
-
-
-
 function Hero16() {
 
 
